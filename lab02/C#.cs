92d9d50617853ada21f127fc06c5fb81d0980607
palmnatthakarn <118332991+palmnatthakarn@users.noreply.github.com>
2023-01-07 15:13:00 +0700

    Create C#.cs

---
+++ lab02/C#.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Name
+{
+    public static void Main (string[] args)
+    {
+    
+        string name = "Natthakarn";
+        string StudentID = "65543206010-2";
+
+        Console.WriteLine ("my name is : {0}",name);
+        Console.WriteLine ("StudentID : {0}", StudentID);
+    }
+}
