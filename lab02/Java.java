f6e1e6bb2e29c8809bbf9cb19329815225f29e84
palmnatthakarn <118332991+palmnatthakarn@users.noreply.github.com>
2023-01-07 15:34:09 +0700

    Create Java.java

---
+++ lab02/Java.java
@@ -0,0 +1,9 @@
+class name {
+    public static void main(String[] args) {
+        
+        String firstName = "Natthakarn";
+        String lastName = "Kaewsamtor";
+        
+        System.out.println(firstName + " " + lastName);
+    }
+}
